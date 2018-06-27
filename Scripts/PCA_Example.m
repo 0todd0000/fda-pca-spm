c6f5dabc4a8f034f31a1fc0c81d47ff0c5bef962
Todd Pataky <0todd0@gmail.com>
2018-06-28 07:29:06 +0900

    Updating PCA script
    Updating PCA script

--- Scripts/PCA_Example.m
+++ Scripts/PCA_Example.m
@@ -7,101 +7,57 @@
 clear; clc   %clear workspace
 close all    %close all figures
 
+
+%% load and combine data into a single array
 load('Warmenhoven2018-bow-force.mat');
-
 Y = [Pin_Force_Bow_Male Pin_Force_Bow_Female]';
-t = linspace(0, 100, 51)'; %time
+t = linspace(0, 100, 51)';  %time
 
 
-% The principal components themselves are time-series and can be observed 
-% graphically. See the figures below. figure(105) shows PC1 and figure(106)
-% shows PC2. 
+%% Run PCA
+[coeff,score,latent,tsquared,explained] = pca(Y);
 
-figure(105)
-plot(x(:,1));
 
-figure(106)
-plot(x(:,2));
 
-% The PCs can also be observed from a more practical perspective by adding
-% and substracting them from the average/mean curve. To do this, first the
-% mean curve is calculated:
-
-mean_curve = mean(Bow_Side_Data);
-
-% Then a constant is created to scale each of the PCs before adding and
-% substracting them from the mean. A constant that is commonly used is 1 or
-% 2 standard deviations (SD) of the PC scores. So first the SD of PC scores
-% is calculated for each PC. 
-
-stdevscores = std(y);
-
-% Then this is used to create the constant:
-
-con1 = stdevscores(1)*1;
-
-% And then scaled PCs are added and subtracted from the mean curve, before
-% being plotted with the mean curve. 
-
-PC1_Pos = mean_curve' + con1.*(x(:,1));
-PC1_Neg = mean_curve' - con1.*(x(:,1));
-
-% Figure(107) shows PC1, with positive scorers plotted using the '+'
-% symbols and negative scorers plotted using the '-' symbols. 
-
-figure(107)
-phdl = plot(time, mean_curve', 'k-');
-set(phdl, 'LineWidth', 1.5)
+%% Visualize first two PCs
+figure(1)
+pc1 = coeff(:,1);
+pc2 = coeff(:,2);
+plot(t, pc1)
 hold on
-phdl = text(time, PC1_Pos, '+');
-set(phdl, 'LineWidth', 1.5)
+plot(t, pc2)
+legend('PC-1', 'PC-2')
+
+
+%% Visualize first PC in the context of the overall mean trajectory
+figure(2)
+ymean    = mean(Y, 1)';
+scoresd  = std(score, [], 1)';
+plot(t, ymean, 'k', 'linewidth', 3)
 hold on
-phdl = text(time, PC1_Neg, '-');
-set(phdl, 'LineWidth', 1.5)
-hold off
-box on
-set(gca,'FontSize',10);
-title('\fontsize{10} PC1');
-xlabel('\fontsize{10} Drive Phase (%)')
-ylabel('\fontsize{10} Force (N)')
-axis([0,100,-100,700])
+plot(t, ymean + scoresd(1) .* pc1, 'b+')
+plot(t, ymean - scoresd(1) .* pc1, 'bv')
+title('PC-1')
 
-% The same process is also carried out for PC2. Inclusive of creating a
-% constant to scale the PCs.
 
-con2 = stdevscores(2)*1;
-
-PC2_Pos = mean_curve' + con2.*(x(:,2));
-PC2_Neg = mean_curve' - con2.*(x(:,2));
-
-figure(108)
-phdl = plot(time, mean_curve', 'k-');
-set(phdl, 'LineWidth', 1.5)
+%% Visualize second PC in the context of the overall mean trajectory
+figure(3)
+plot(t, ymean, 'k', 'linewidth', 3)
 hold on
-phdl = text(time, PC2_Pos, '+');
-set(phdl, 'LineWidth', 1.5)
-hold on
-phdl = text(time, PC2_Neg, '-');
-set(phdl, 'LineWidth', 1.5)
-hold off
-box on
-set(gca,'FontSize',10);
-title('\fontsize{10} PC2');
-xlabel('\fontsize{10} Drive Phase (%)')
-ylabel('\fontsize{10} Force (N)')
-axis([0,100,-100,700])
+plot(t, ymean + scoresd(2) .* pc2, 'b+')
+plot(t, ymean - scoresd(2) .* pc2, 'bv')
+title('PC-2')
 
-% An example of a way to graph the principal component scores can also be 
-% seen below, with males in blue and females in red for PC1 and PC2. 
 
-figure(109)
-scatter(y(1:10,1),y(1:10,2), 'b');
+%% Visualize first two PC scores
+figure(4)
+plot( score(1:10,1), score(1:10,2), 'bo' );
 hold on
-scatter(y(11:20,1),y(11:20,2), 'r');
-hold off
-box on
-set(gca,'FontSize',10);
-title('\fontsize{10} PC1 vs PC2');
-xlabel('\fontsize{10} PC1 Scores')
-ylabel('\fontsize{10} PC2 Scores')
-axis([-1000,1000,-400,400])
+plot( score(11:20,1), score(11:20,2), 'ro' );
+plot( score(11:20,1), score(11:20,2), 'ro' );
+xlabel('PC-1')
+ylabel('PC-2')
+legend('males', 'females')
+ax = gca();
+plot([0 0], get(ax,'ylim'), 'k:')
+plot(get(ax,'xlim'), [0 0], 'k:')
